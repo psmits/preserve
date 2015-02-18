b569b15456af317e54b520b2eb405c2fdd5b4525
psmits <thestinkypete@gmail.com>
2015-02-18 00:43:51 -0600

    various random notes and lit list added to intro. fixed error in hierarchical model

--- R/post_sim.r
+++ R/post_sim.r
@@ -1,5 +1,23 @@
 library(rstan)
 library(plyr)
 
+RNGkind(kind = "L'Ecuyer-CMRG")
+set.seed(420)
+nsim <- 1000
+
+# misc functions
+# poisson/negative binomial residuals
+
+
+# empirical data sent to stan
+souce('../R/mung.r')  # raw data
+emp <- read_rdump('../data/data_dump/count_info.data.R')
+
+# csv of marginal posteriors from stan
 np <- list.files('../data/mcmc_out/', pattern = 'samp', full.names = TRUE)
 preserve.fit <- read_stan_csv(np)
+
+
+# for ii in seq(nsim))
+#   extract random draw from marginal posteriors
+#   generate fake data given known covariates
