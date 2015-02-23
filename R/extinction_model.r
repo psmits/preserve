1211f8f5807e7a3976a38151da9776e55b2f6a20
psmits <thestinkypete@gmail.com>
2015-02-23 16:48:18 -0600

    wrote stan model for phanerozoic extinction. included biotic group + cohort + macro regime

--- R/extinction_model.r
+++ R/extinction_model.r
@@ -5,10 +5,49 @@ library(parallel)
 source('../R/mung.r')
 
 # age.data
+num.samp <- nrow(age.data)
+num.orig <- length(unique(age.data$orig))
+num.regime <- length(unique(age.data$regime))
+num.class <- length(unique(age.data$class))
+
+con.orig <- match(age.data$orig, gts)
+con.class <- as.numeric(as.factor(age.data$class))
+con.regime <- as.numeric(age.data$regime)
+
+
+data <- list(duration = age.data$duration, group = con.class,
+             cohort = con.orig, regime = con.regime)
+
+dead <- age.data$censored != 1
+unc <- llply(data, function(x) x[dead])
+cen <- llply(data, function(x) x[!dead])
+
+data <- list(dur_unc = unc$duration,
+             group_unc = unc$group,
+             cohort_unc = unc$cohort,
+             regime_unc = unc$regime,
+             N_unc = length(unc$duration),
+             dur_cen = cen$duration,
+             group_cen = cen$group,
+             cohort_cen = cen$cohort,
+             regime_cen = cen$regime,
+             N_cen = length(cen$duration))
+
+data$samp_unc <- seq(data$N_unc)
+data$samp_cen <- seq(from = data$N_unc + 1,
+                     to = data$N_unc + data$N_cen,
+                     by = 1)
+
+data$N <- num.samp
+data$O <- num.orig
+data$R <- num.regime
+data$C <- num.class
+data$regime <- unique(age.data[, c('orig', 'regime')])[, 2]
+
+
+stan(file = '../stan/survival_model.stan')
 
 # remove youngest cohort?
-
-# make cohort, regime, and class into numerics
 
 # stan model
 # duration ~ Weibull(alpha, exp(-(intercept + class membership + cohort)
---
+++ stan/survival_model.stan
@@ -0,0 +1,66 @@
+data {
+  int N;
+  int O;
+  int R;
+  int C;
+  int N_unc;
+  int N_cen;
+  int samp_unc[N_unc];
+  int samp_cen[N_cen];
+  real<lower=0> dur_unc[N_unc];
+  int group_unc[N_unc];
+  int cohort_unc[N_unc];
+  int regime_unc[N_unc];
+  real<lower=0> dur_cen[N_cen];
+  int group_cen[N_cen];
+  int cohort_cen[N_cen];
+  int regime_cen[N_cen];
+
+  int regime[O];
+}
+transformed data {
+}
+parameters {
+  real<lower=0> alpha;
+  real intercept;
+
+  real group[C];
+  real<lower=0> sigma_group;
+
+  real cohort[O];
+  real cohort_mu[R];
+  real<lower=0> sigma_cohort;
+  real<lower=0> sigma_regime;
+}
+model {
+  alpha ~ cauchy(0, 2.5);
+  intercept ~ normal(0, 10);
+
+  for(c in 1:C) {
+    group[c] ~ normal(0, sigma_group);
+  }
+  sigma_group ~ cauchy(0, 2.5);
+
+  for(o in 1:O) {
+    cohort[o] ~ normal(cohort_mu[regime[o]], sigma_cohort);
+  }
+  sigma_cohort ~ cauchy(0, 2.5);
+
+  for(r in 1:R) {
+    cohort_mu[r] ~ normal(0, sigma_regime);
+  }
+  sigma_regime ~ cauchy(0, 2.5);
+
+  for(i in 1:N_unc) {
+    increment_log_prob(weibull_log(dur_unc[i], alpha, 
+          exp(-(intercept + group[group_unc[i]] + 
+          cohort[cohort_unc[i]]) / alpha)));
+  }
+  for(i in 1:N_cen) {
+    increment_log_prob(weibull_ccdf_log(dur_cen[i], alpha,
+          exp(-(intercept + group[group_cen[i]] + 
+          cohort[cohort_cen[i]]) / alpha)));
+  }
+}
+generated quantities {
+}
