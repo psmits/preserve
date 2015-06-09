5f8aa765135e2716ce150c9b205408444de70c29
psmits <thestinkypete@gmail.com>
2015-06-09 16:31:09 -0500

    probabilities of correlations

--- R/extinction_analysis.r
+++ R/extinction_analysis.r
@@ -1,1 +1,17 @@
+library(plyr)
+
 load('../data/epi_over_off.rdata')
+
+cor.mean <- matrix(, ncol = 5, nrow = 5)
+for(ii in seq(5)) {
+  for(jj in seq(5)) {
+    cor.mean[jj, ii] <- mean(wei.fit$Omega[, jj, ii])
+  }
+}
+rownames(cor.mean) <- c('i', 'r', 'e', 'e2', 'm')
+colnames(cor.mean) <- c('i', 'r', 'e', 'e2', 'm')
+
+ll <- dim(wei.fit$Omega)[1]
+p.cor.ir <- sum(wei.fit$Omega[, 1, 2] < 0) / ll
+p.cor.ie2 <- sum(wei.fit$Omega[, 1, 4] < 0) / ll
+p.cor.re2 <- sum(wei.fit$Omega[, 2, 4] > 0) / ll
