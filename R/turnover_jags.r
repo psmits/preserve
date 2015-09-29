5f05240822d15b9a843e9fe09ee6834e46a7bc22
psmits <thestinkypete@gmail.com>
2015-09-29 11:28:47 -0500

    updated for jags via command line instead of embedded in r

--- R/turnover_jags.r
+++ R/turnover_jags.r
@@ -4,34 +4,12 @@ library(parallel)
 library(rjags)
 library(coda)
 library(rstan)
-#load.module('glm')
-#source('../R/gts.r')
-#source('../R/mung.r')
-#
-#set.seed(420)
-#
-#data.file <- list.files('../data', pattern = 'Occs')
-#fossil <- read.csv(paste0('../data/', data.file))
-#bibr <- fossil
-#
-#lump.file <- list.files('../data', pattern = 'lump')
-#lump <- read.csv(paste0('../data/', lump.file))
-#
-#shape <- readShapeSpatial('../data/ne_10m_coastline.shp')  # from natural earth
-#
-#sight <- space.time(bibr, 
-#                    bins = 'StageNewOrdSplitNoriRhae20Nov2013', 
-#                    gts = rev(as.character(lump[, 2])),
-#                    cuts = 'Chang',
-#                    bot = 'Trem',
-#                    shape = shape)
-#save.image('../data/gradient_setup.rdata')
 load('../data/gradient_setup.rdata')
 n <- 4
 sight <- sight[1:n]
 
 sizes <- laply(sight, dim)
-time.bin <- sizes[1, 2]
+time.bin <- as.numeric(sizes[1, 2])
 prov.size <- sizes[, 1]
 
 taxon <- as.numeric(as.factor(Reduce(c, llply(sight, rownames))))
@@ -44,33 +22,16 @@ for(ii in seq(length(sight))) {
   total.occur[match(rownames(sight[[ii]]), total.names), , ii] <- sight[[ii]]
 }
 
-data <- list(R = ntaxa, C = time.bin, J = length(sight), 
-             sight = total.occur)
+data <- list(nindiv = ntaxa, 
+             nyear = time.bin, 
+             nprov = length(sight), 
+             y = total.occur)
+
+
 save(data, file = '../data/data_dump/occurrence_data.rdata')
+with(data, {dump(c('nindiv', 'nyear', 'nprov', 'y'), 
+                file = '../data/data_dump/occurrence_dump.R')})
 
-p.init <- array(0, dim = c(data$C, data$J))
-
-jags <- with(data, {jags.model('../jags/hmm_hierarchical.jags', 
-                               data = list('nprov' = J,
-                                           'nyear' = C, 
-                                           'nindiv' = R,
-                                           'y' = sight),
-                               n.chains = 4,
-                               n.adapt = 100,
-                               inits = list(z = sight,
-                                            p_norm = p.init))})
-# warm-up/burn-in
-update(jags, 10000)
-# production
-post.samp <- coda.samples(jags, c('psi', 
-                                  'gamma', 'phi', 'p',
-                                  'gamma_mu', 'phi_mu', 'p_mu',
-                                  'gamma_sigma', 'phi_sigma', 'p_sigma',
-                                  'gamma_group', 'phi_group', 'p_group',
-                                  'gamma_sigma_group', 
-                                  'phi_sigma_group', 
-                                  'p_sigma_group',
-                                  'beta', 'alpha',
-                                  'z', 'turnover'),  
-                          n.iter = 10000, thin = 10)
-save(post.samp, file = '../data/mcmc_out/turnover_jags.rdata')
+p <- array(0, dim = c(data$nyear, data$nprov))
+z <- data$y
+dump(c('p', 'z'), file = '../data/data_dump/hmm_inits.R')
