a6f9bda38f8f497447b31f071d995664aeffd51e
Peter Smits <thestinkypete@gmail.com>
2018-02-21 14:07:08 -0800

    tidy cleaning. issues with macrostrat

--- R/data_process.r
+++ R/data_process.r
@@ -1,15 +1,18 @@
 library(rstan)
 library(rstanarm)
+library(SpadeR)
+
 library(tidyverse)
+
 library(sp)
 library(dismo)
 library(raster)
 library(rgdal)
 library(XML)
 library(maptools)
-library(SpadeR)
+
 source('../R/gts.r')
-#source('../R/jade.r')
+source('../R/jade.r')
 #source('../R/mung.r')
 
 # geologicl stage information
@@ -20,6 +23,7 @@ timeframe <- c('Trem', 'Floi', 'Dapi', 'Darr', 'Sand', 'Kati', 'Hirn', 'Ldov',
                'Fras', 'Fame', 'Tour', 'Vise', 'Serp', 'Bash', 'Mosc', 'Step', 
                'Asse', 'Sakm', 'Arti', 'Kung', 'Road', 'Word', 'Capi', 'Wuchi', 
                'Chang')
+timecount <- lump[lump[, 2] %in% timeframe, 1]
 
 # raw occurrence data from pbdb
 ff <- read.csv('https://paleobiodb.org/data1.2/occs/list.txt?base_name=Brachiopoda&interval=Ordovician,Permian&show=full', 
@@ -31,29 +35,39 @@ bibr <- fossil
 
 # place nice
 names(bibr) <- str_replace_all(names(bibr), '\\_', '\\.')
-
 # just brachiopod orders
 bo <- unique(ff$order)
 bo <- bo[bo != '']
 
 # play nice
-bibr <- mutate(bibr, stagename = StageNewOrdSplitNoriRhae20Nov2013)
-bibr <- mutate(bibr, stagenum = lump[match(stagename, lump[, 2]), 1])
+bibr <- dplyr::mutate(bibr, stagename = StageNewOrdSplitNoriRhae20Nov2013)
+bibr <- dplyr::mutate(bibr, stagenum = lump[match(stagename, lump[, 2]), 1])
+bibr <- filter(bibr, !is.na(stagenum))
 
-# TO DO
+# survivors
+bibr <- group_by(bibr, occurrences.genus.name) %>%
+  dplyr::mutate(survivor = any(stagenum > max(timecount))) %>%
+  ungroup()
+
+
 # only species originating in timeframe
-
-
-
 # filter down to brachiopods with info
 bibr <- filter(bibr, 
+               stagenum >= min(timecount), 
+               stagenum <= max(timecount),
                occurrences.order.name %in% bo,  # taxa
                !is.na(collections.paleolngdec),  # location
                !is.na(collections.paleolatdec),
                !is.na(EO.5.1.2014),  # env
-               !is.na(stagename),
                stagename %in% timeframe)  # timing
 bibr$stagename <- factor(bibr$stagename, levels = timeframe)
+
+
+
+# age in # stages incl FAD, LAD
+bibr <- group_by(bibr, occurrences.genus.name) %>%
+  dplyr::mutate(duration = abs(max(stagenum) - min(stagenum)) + 1) %>%
+  ungroup()
 
 # put points on map
 eq <- CRS("+proj=cea +lat_0=0 +lon_0=0 +lat_ts=30 +a=6371228.0 +units=m")
@@ -73,94 +87,83 @@ bibr$cell <- cellFromXY(ras,
 # how many cells are in each stage
 #   world isn't sampled completely, so range size prop to world
 bibr <- group_by(bibr, stagename) %>%
-  dplyr::mutate(world = n_distinct(cell))
-
-# range size at time
-bibr <- group_by(bibr, stagename, occurrences.genus.name) %>%
-  dplyr::mutate(range = n_distinct(cell), rangeprop = range / world) %>%
+  dplyr::mutate(world = n_distinct(cell)) %>%
+  group_by(occurrences.genus.name, add = TRUE) %>%
+  # range size at time and more
+  dplyr::mutate(range = n_distinct(cell), 
+                rangeprop = range / world) %>% 
   ungroup()
 
-# JADE correction of range proportion
+# JADE correction of range proportion and more
+# occurrence environment
 bibr <- group_by(bibr, stagename) %>%
-  transmute(chaorange = DetInc(c(unique(world), range))) %>%
+  dplyr::mutate(chaorange = DetInc(c(unique(world), range))) %>%
   ungroup()
 
-# pick up here
+# time specific occurrence types
+bibr <- group_by(bibr, stagename) %>%
+  dplyr::mutate(edge.world = sum(EO.5.1.2014 == 'E'),  
+                off.world = sum(EO.5.1.2014 == 'O')) %>%
+  # genus time specific occurrence types
+  group_by(occurrences.genus.name, add = TRUE) %>%
+  dplyr::mutate(edge = sum(EO.5.1.2014 == 'E'),  
+                off = sum(EO.5.1.2014 == 'O')) %>%
+  ungroup()
 
+# i now need to create two data frames
+#   survival data frame
+#     duration
+#     genus identity
+#   longitudinal dataframe
+#     range at time
+#     genus identity
+bibr <- group_by(bibr, occurrences.genus.name) %>% 
+  dplyr::mutate(start = min(stagenum)) %>% 
+  ungroup()
 
-#ncel <- ddply(bibr, .(occurrences.genus.name, stagenum), 
-#              summarize, tt = length(unique(cell)))
-#ncel.stage <- split(ncel, ncel[, 2])
-#sum(ncel.stage[[1]][, 3])
-
-focus <- dplyr::select(bibr, 
-                       occurrences.genus.name, 
-                       age,
-                       start,
+bibr <- dplyr::mutate(bibr, 
+                      id = factor(occurrences.genus.name, 
+                                  levels = unique(occurrences.genus.name)),
+                      id = as.numeric(id))
+bibr <- dplyr::arrange(bibr, id)
+survi <- unique(dplyr::select(bibr, 
+                              id, 
+                              duration, 
+                              survivor)) %>%
+  dplyr::mutate(dead = (!survivor) * 1)
+longi <- dplyr::select(bibr, 
+                       id,
+                       start, 
                        stagename, 
                        stagenum, 
-                       range, 
-                       world, 
-                       rangeprop)
+                       chaorange) %>%
+  dplyr::mutate(timing = stagenum - start + 1,
+                scalechaorange = arm::logit(chaorange))
 
 
+# connect to macrostrat
+uc <- unique(bibr$collection.no)
+fc <- uc[1:(length(uc) / 2)]
+sc <- uc[((length(uc) / 2) + 1):length(uc)]
+furl <- paste0('https://macrostrat.org/api/v2/fossils?cltn_id=', 
+               paste0(fc, collapse = ','), 
+               '&response=long&format=csv')
+strat1 <- read.csv(furl, stringsAsFactors = FALSE)
+furl <- paste0('https://macrostrat.org/api/v2/fossils?cltn_id=', 
+               paste0(sc, collapse = ','), 
+               '&response=long&format=csv')
+strat2 <- read.csv(furl, stringsAsFactors = FALSE, row.names = NULL)
+# combine into one
+strat <- dplyr::union(strat1, strat2)
 
+# mid point age of unit
+# unit duration
+strat <- dplyr::mutate(strat, 
+                       m_age = map2_dbl(t_age, b_age, ~ mean(Reduce(c, .x, .y))),
+                       duration = abs(t_age - b_age))
 
+# the goal is connect macrostrat to occurrence information
+# collections are in units with continuous time
+#   1 My bins?
+# assign all occurrences a time
 
-payne <- read.table('../data/payne_bodysize/Occurrence_PaleoDB.txt',
-                    header = TRUE, stringsAsFactors = FALSE)
-
-taxon = c('Rhynchonellata', 'Strophomenata', 'Chileata', 'Obolellata', 
-          'Kutorginata', 'Spiriferida', 'Spiriferinida')
-bins = 'StageNewOrdSplitNoriRhae20Nov2013'
-gts = rev(as.character(lump[, 2]))
-cuts = 'Chang'
-bot = 'Trem'
-short.data <- sort.data(bibr, payne, taxon = taxon,
-                        bins = bins,
-                        gts = rev(as.character(lump[, 2])),
-                        cuts = cuts,
-                        bot = bot)
-
-ss <- bibr[bibr$occurrences.genus_name %in% short.data$genus, ]
-ss <- unique(ss[, c('occurrences.genus_name', 'occurrences.species_name')])
-ss <- ss[order(ss[, 1]), ]
-ss <- ss[!(str_detect(ss[, 2], 'sp') | str_detect(ss[, 2], '[A-Z]')), ]
-ss.gen <- ddply(ss, .(occurrences.genus_name), summarize, 
-                o <- length(occurrences.species_name))
-
-# now just set up data
-num.samp <- nrow(short.data)
-
-con.orig <- match(as.character(short.data$orig), rev(as.character(lump[, 2])))
-con.orig <- mapvalues(con.orig, from = unique(con.orig), 
-                      to = rank(unique(con.orig)))
-num.orig <- length(unique(con.orig))
-
-
-# assemble standata
-standata <- list(y = short.data$duration, 
-                 status = short.data$censored,
-                 cohort = con.orig)
-
-standata$N <- num.samp
-standata$O <- num.orig
-
-# covariates
-K <- 3
-X <- matrix(1, ncol = K, nrow = standata$N)
-X[, 2] <- rescale(logit(short.data$occupy))
-X[, 3] <- rescale(log(short.data$size))
-standata$X <- X
-standata$K <- ncol(standata$X)
-
-# export
-with(standata, {stan_rdump(list = c('N', 
-                                    'O',
-                                    'K',
-                                    'y', 
-                                    'X',
-                                    'status', 
-                                    'cohort'
-                                    ),
-                           file = '../data/data_dump/clean_info.data.R')})
